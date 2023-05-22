fd713997bbb84f0b7cd8bda9b2d1bd1133cafd45
Hong Shik Branden Kim <kimploo@gmail.com>
2023-05-22 15:42:26 +0800

    make some page for sample

---
+++ src/pages/Entry.tsx
@@ -0,0 +1,20 @@
+/**
+ * @name wwm01
+ *
+ */
+
+export default function Entry() {
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">감정지도</h1>
+            <p className="py-6">Your Message Here</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
