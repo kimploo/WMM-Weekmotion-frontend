6dfd0a42b187a092d401b0adc419ca7af7762ac4
iuesver <87600354+iuesver@users.noreply.github.com>
2023-05-26 16:51:17 +0900

    feat : 페이지 진입 시 모달박스 추가

--- src/pages/Scheduler.tsx
+++ src/pages/Scheduler.tsx
@@ -1,15 +1,24 @@
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import Calendar from 'react-calendar';
 import '../assets/customCSS/calendar.css';
-import { useEffect } from 'react';
-import { tab, tabActive } from '../assets/customCSS/designSystem';
+import {
+  tab,
+  tabActive,
+  smBtnYellowBorder,
+  smBtnYellow
+} from '../assets/customCSS/designSystem';
+import weekmotion_ask from '../assets/images/weekmotion_ask.svg';
+import checkBoxEnable from '../assets/images/checkBoxEnable.svg';
+import checkBoxChecked from '../assets/images/checkBoxSelected.svg';
+import closeIcon from '../assets/images/closeIcon.svg';
+import { useState } from 'react';
 
 export default function Schduler() {
   const [tabParams, setTabParams] = useSearchParams({ tab: 'calendar' });
-  useEffect(() => {
-    console.log(tabParams.get('tab'));
-  }, [tabParams]);
+  const [isChecked, setIsChecked] = useState(true);
+  const [isCheckedToday, setIsCheckedToday] = useState(false);
+
   return (
     <section className="h-screen bg-mono-100">
       <div className="w-full h-14 flex justify-around items-center">
@@ -26,6 +35,63 @@ export default function Schduler() {
           목록
         </button>
       </div>
+      {/* modal box */}
+      <input
+        type="checkbox"
+        id="my-modal-3"
+        className="modal-toggle"
+        checked={isChecked}
+        readOnly
+      />
+      <div className="modal">
+        <div className="modal-box relative bg-mono-100">
+          <label
+            htmlFor="my-modal-3"
+            className="btn absolute right-2 top-2 bg-mono-100 border-none hover:bg-mono-100"
+            onClick={() => setIsChecked(!isChecked)}
+          >
+            <img src={closeIcon} alt="close_icon" />
+          </label>
+          <h3 className="text-lg font-bold text-mono-700">
+            오늘의 감정을
+            <br />
+            기록하러 갈까요?
+          </h3>
+          <img src={weekmotion_ask} alt="ask_icon" className="w-full px-5" />
+          <div className="flex justify-end gap-x-[7px] pr-1 pb-[9px]">
+            <span className="text-mono-700">오늘 하루 보지않기</span>
+            <label
+              htmlFor="checkToday"
+              onClick={() => setIsCheckedToday(!isCheckedToday)}
+            >
+              {!isCheckedToday ? (
+                <img src={checkBoxEnable} alt="enable_icon" />
+              ) : (
+                <img src={checkBoxChecked} alt="checked_icon" />
+              )}
+            </label>
+            <input
+              type="checkbox"
+              name="checkToday"
+              id="checkToday"
+              className="hidden"
+              readOnly
+            />
+          </div>
+          <div className="flex justify-end gap-x-2">
+            <label
+              htmlFor="my-modal-3"
+              className={smBtnYellowBorder}
+              onClick={() => setIsChecked(!isChecked)}
+            >
+              안할래요
+            </label>
+            <Link to={'/before'}>
+              <button className={smBtnYellow}>기록하기</button>
+            </Link>
+          </div>
+        </div>
+      </div>
       {tabParams.get('tab') === 'calendar' ? (
         <Calendar
           formatDay={(locale, date) => {
@@ -36,7 +102,7 @@ export default function Schduler() {
           className="text-mono-700"
         />
       ) : (
-        <div></div>
+        <div>List here</div>
       )}
     </section>
   );
