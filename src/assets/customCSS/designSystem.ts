e7b460e05fce7233f33f97d88f3410bbb0bae18d
iuesver <87600354+iuesver@users.noreply.github.com>
2023-05-26 16:50:10 +0900

    feat : 이미지 및 스타일 코드 추가

--- src/assets/customCSS/designSystem.ts
+++ src/assets/customCSS/designSystem.ts
@@ -8,7 +8,14 @@ export const btnGray =
 export const btnGrayBorder =
   'btn w-full h-14 border-2 rounded-full bg-mono-100 border-mono-300 font-sans text-mono-300 text-base font-bold';
 // small button css
-
+export const smBtnYellow =
+  'btn w-[103px] h-10 rounded-full bg-emotion-yellow border-emotion-yellow font-sans text-mono-100 text-sm font-bold';
+export const smBtnYellowBorder =
+  'btn w-[103px] h-10 border-2 rounded-full bg-mono-100 border-emotion-yellow font-sans text-emotion-yellow text-sm font-bold';
+export const smBtnGray =
+  'btn w-[103px] h-10 rounded-full bg-mono-300 border-mono-300 font-sans text-mono-100 text-sm font-bold';
+export const smBtnGrayBorder =
+  'btn w-[103px] h-10 border-2 rounded-full bg-mono-100 border-mono-300 font-sans text-mono-300 text-sm font-bold';
 // chips css
 export const chipsPink =
   'badge h-7 bg-emotion-lightPink border-emotion-lightPink text-mono-700 cursor-pointer p-3';
