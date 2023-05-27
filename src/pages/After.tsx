b4b1ce44642e7bc833817d0d3581daac26b757e8
iuesver <87600354+iuesver@users.noreply.github.com>
2023-05-27 15:36:33 +0900

    feat : emotionSlice state 변경

--- src/pages/After.tsx
+++ src/pages/After.tsx
@@ -1,28 +1,60 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { change } from '../redux/slice/emotionSlice';
 import weekmotion from '../assets/images/weekmotion.svg';
-import { positiveEmotion, negativeEmotion } from '../assets/strings/emotions';
 import { RootState } from '../redux';
+import axios from 'axios';
+import { BASE_URL } from '../redux/function/url';
+import { tag } from '../redux/types';
+import {
+  chipsBlue,
+  chipsBlueBorder,
+  chipsPink,
+  chipsPinkBorder
+} from '../assets/customCSS/designSystem';
 
 export default function After() {
   const state = useSelector((state: RootState) => {
     return state.emotion;
   });
-  const [emotion, setEmotion] = useState<string[]>(state.emotion);
+  const [emotion, setEmotion] = useState<tag[]>([]);
+  const [checkedEmotion, setCheckedEmotion] = useState<any[]>(state.emotion);
   const dispatch = useDispatch();
 
   const onClick = (event: React.MouseEvent<HTMLDivElement>) => {
     const { innerText } = event.target as HTMLDivElement;
-    if (!emotion.includes(innerText) && emotion.length < 3) {
-      setEmotion([...emotion, innerText]);
+    if (
+      !checkedEmotion.includes(
+        emotion.find((item: tag) => item.tagName === innerText)
+      ) &&
+      checkedEmotion.length < 3
+    ) {
+      setCheckedEmotion([
+        ...checkedEmotion,
+        emotion.find((item: tag) => item.tagName === innerText)
+      ]);
     } else {
-      setEmotion(emotion.filter((item: string) => item !== innerText));
+      setCheckedEmotion(
+        checkedEmotion.filter((item: tag) => item.tagName !== innerText)
+      );
     }
   };
+
+  const requestEmotion = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/tag`);
+      setEmotion(response.data.data);
+    } catch (error) {
+      throw new Error('Error!');
+    }
+  };
+
+  useEffect(() => {
+    requestEmotion();
+  }, []);
 
   return (
     <section className="bg-mono-100 h-screen flex flex-col items-center">
@@ -35,37 +67,62 @@ export default function After() {
           <br />
           오늘의 감정이 바뀌었나요?
         </h1>
-        <div className="my-4">
-          {positiveEmotion.map((item: string, index: number) => (
-            <div
-              key={index}
-              className={
-                emotion.includes(item)
-                  ? 'badge bg-emotion-lightPink border-emotion-lightPink text-mono-700 cursor-pointer m-1 p-3'
-                  : 'badge badge-outline border-emotion-lightPink text-mono-700 cursor-pointer m-1 p-3'
-              }
-              onClick={onClick}
-            >
-              {item}
+        {emotion && (
+          <>
+            <div className="my-4">
+              {emotion
+                .filter((data) => data.tagCategory.seq === '1')
+                .map((item: tag, index: number) => (
+                  <div
+                    key={index}
+                    className={
+                      checkedEmotion.includes(item)
+                        ? chipsPink
+                        : chipsPinkBorder
+                    }
+                    onClick={onClick}
+                  >
+                    {item.tagName}
+                  </div>
+                ))}
             </div>
-          ))}
-        </div>
-        <div className="my-4">
-          {negativeEmotion.map((item: string, index: number) => (
-            <div
-              key={index}
-              className={
-                emotion.includes(item)
-                  ? 'badge bg-emotion-lightBlue border-emotion-lightBlue cursor-pointer text-mono-700 m-1 p-3'
-                  : 'badge badge-outline border-emotion-lightBlue cursor-pointer text-mono-700 m-1 p-3'
-              }
-              onClick={onClick}
-            >
-              {item}
+            <div className="my-4">
+              {emotion
+                .filter((data: tag) => data.tagCategory.seq === '2')
+                .map((item: tag, index: number) => (
+                  <div
+                    key={index}
+                    className={
+                      checkedEmotion.includes(item)
+                        ? chipsBlue
+                        : chipsBlueBorder
+                    }
+                    onClick={onClick}
+                  >
+                    {item.tagName}
+                  </div>
+                ))}
             </div>
-          ))}
-        </div>
-        <Link to={'/post'} onClick={() => dispatch(change(emotion))}>
+            <div className="my-4">
+              {emotion
+                .filter((data: tag) => data.tagCategory.seq === '3')
+                .map((item: tag, index: number) => (
+                  <div
+                    key={index}
+                    className={
+                      checkedEmotion.includes(item)
+                        ? chipsBlue
+                        : chipsBlueBorder
+                    }
+                    onClick={onClick}
+                  >
+                    {item.tagName}
+                  </div>
+                ))}
+            </div>
+          </>
+        )}
+        <Link to={'/post'} onClick={() => dispatch(change(checkedEmotion))}>
           <button className="btn w-full rounded-full bg-emotion-yellow border-emotion-yellow text-mono-100">
             감정 선택하기
           </button>
