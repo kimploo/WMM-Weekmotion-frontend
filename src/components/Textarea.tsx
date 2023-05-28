478796aa6c6f05166259a0376e195b5f16e66466
Hong Shik Branden Kim <kimploo@gmail.com>
2023-05-28 09:54:59 +0800

    apply custom input

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -2,33 +2,33 @@ interface Props {
   name: string;
   label: string;
   id?: string;
-  type?: string;
   value?: string;
   placeholder: string;
   required: boolean;
-  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement> | undefined;
 }
 
-export default function Input({
+export default function Textarea({
   name = '',
   label = '',
   id = '',
-  type = 'text',
   required = false,
   placeholder = '',
   onChange
 }: Props) {
   return (
-    <div className="relative h-14 bg-mono-100 text-mono-700 border-2 border-solid border-[#DCDCDC] rounded-lg">
+    <div
+      className="relative mt-4 h-1/2 bg-mono-100 text-mono-700 border-2 border-solid border-[#DCDCDC] rounded-lg
+    resize-none"
+    >
       <label
         className="absolute px-1 z-10 -top-2.5 left-2 text-xs font-normal text-[#ACACAC] bg-mono-100"
         htmlFor={label}
       >
         {label}
       </label>
-      <input
-        className="block w-full pl-4 h-14 border-2 border-transparent appearance-none bg-transparent"
-        type={type}
+      <textarea
+        className="block w-full pl-4 pt-3 h-full border-transparent appearance-none bg-transparent"
         name={name}
         id={id}
         onChange={onChange}
